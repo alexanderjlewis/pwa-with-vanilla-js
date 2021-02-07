207de338745e93701b277afdc7e1da743d856c15
alexanderjlewis <alex.lewis1991@gmail.com>
2021-02-07 12:21:18 +1100

    commit

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,12 +2,17 @@ const staticDevRecipes = "dev-recipes-site-v1";
 const assets = [
   "/",
   "/index.html",
+  "/bagels.html",
+  "/ragu.html",
   "/js/app.js",
+  "/css/bootstrap.min.css",
+  "/js/bootstrap.bundle.min.js",
+  "/js/gitgraph.umd.js",
 ];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevrecipes).then(cache => {
+    caches.open(staticDevRecipes).then(cache => {
       cache.addAll(assets);
     })
   );
