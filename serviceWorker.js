8f2adc15f4b2c7dd5c83c12b89dd10129397e5cf
alexanderjlewis <alex.lewis1991@gmail.com>
2021-02-08 22:32:41 +1100

    commit

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,28 +1,30 @@
 const staticJOLRecipes = "JOL-recipes-site-v1";
 const assets = [
-  "/",
-  "/index.html",
-  "/list.html",
-  "/bagels.html",
-  "/ragu.html",
-  "/js/app.js",
-  "/css/bootstrap.min.css",
-  "/js/bootstrap.bundle.min.js",
-  "/js/gitgraph.umd.js",
+    "/",
+    "/index.html",
+    "/list.html",
+    "/bagels.html",
+    "/ragu.html",
+    "/js/app.js",
+    "/css/bootstrap.min.css",
+    "/custom.min.css",
+    "/js/bootstrap.min.js",
+    "/js/gitgraph.umd.js",
+    "/js/jquery-3.5.1.slim.min.js"
 ];
 
 self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticJOLRecipes).then(cache => {
-      cache.addAll(assets);
-    })
-  );
+    installEvent.waitUntil(
+        caches.open(staticJOLRecipes).then(cache => {
+            cache.addAll(assets);
+        })
+    );
 });
 
 self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
-    })
-  );
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request);
+        })
+    );
 });
