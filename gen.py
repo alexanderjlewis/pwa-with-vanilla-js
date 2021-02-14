16b83e9ca4774096d867e70792df2596ff624da9
alexanderjlewis <alex.lewis1991@gmail.com>
2021-02-14 19:40:09 +1100

    wip

--- gen.py
+++ gen.py
@@ -12,9 +12,12 @@ svg = ET.Element('svg', attrib={'viewBox':'0 0 500 500'})
 recipe = data['recipes'][0]
 
 for i, step in enumerate(recipe['steps']):
-    print(step)
+    #print(step)
     #print(len(recipe['steps']))
-    circle = ET.Element('circle', attrib={'cx': '5','cy': str(5 + i*20),'r': '5','fill': 'Green'})
+    circle = ET.Element('circle', attrib={'cx': '10','cy': str(10 + i*20),'r': '5','fill': 'Green'})
     svg.append(circle)
+    text = ET.Element('text', attrib={'x': '25', 'y':str(11 + i*20), 'font-size':'10pt'})
+    text.text = step['instruction']
+    svg.append(text)
 
 ET.ElementTree(svg).write(sys.stdout, encoding='unicode', method='html')
