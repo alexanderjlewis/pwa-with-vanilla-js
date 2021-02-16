385265e9050b77a703e0b46bcbb7595af4e72216
alexanderjlewis <alex.lewis1991@gmail.com>
2021-02-16 22:45:37 +1100

    commit

---
+++ app.py
@@ -0,0 +1,26 @@
+#app.py
+import json
+from gen import generate
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+with open('data/recipes.json') as f:
+    data = json.load(f)
+
+@app.route('/')
+def index_page():
+    return render_template('index.html')
+
+@app.route('/list')
+def list_page():
+    return render_template('list.html', recipes=data['recipes'])
+
+@app.route('/recipe/<name>')
+def hello(name=None):
+    render_recipe = None
+    for recipe in data['recipes']:
+        if recipe['safe_name'] == name:
+            render_recipe = recipe
+    chart = generate(render_recipe)
+    return render_template('chart.html', recipe=render_recipe, chart=chart)
+
--- gen.py
+++ gen.py
@@ -1,23 +1,54 @@
 import sys
 from xml.etree import ElementTree as ET
-import json
 
-with open('recipes.json') as f:
-  data = json.load(f)
+def generate(render_recipe):
+    svg = ET.Element('svg', attrib={'width':'100%', 'height':'100vh'})
 
-#print(data['recipes'][0])
+    node_spacing_y = 60
+    node_r = 13
+    main_start_x = 30
+    main_start_y = 40
+    text_offset_x = 25
+    text_offset_y = -15
+    next_node_y = main_start_y
+    prev_lines = 0
 
-svg = ET.Element('svg', attrib={'viewBox':'0 0 500 500'})
+    if render_recipe:
+        for i, step in enumerate(render_recipe['steps']):
 
-recipe = data['recipes'][0]
+            if i > 0:
+                next_node_y += node_spacing_y + ((prev_lines - 1) * 19)
 
-for i, step in enumerate(recipe['steps']):
-    #print(step)
-    #print(len(recipe['steps']))
-    circle = ET.Element('circle', attrib={'cx': '10','cy': str(10 + i*20),'r': '5','fill': 'Green'})
-    svg.append(circle)
-    text = ET.Element('text', attrib={'x': '25', 'y':str(11 + i*20), 'font-size':'10pt'})
-    text.text = step['instruction']
-    svg.append(text)
+            grad = ET.Element('linearGradient', attrib={'id':('grd'+str(i)), 'x1':'0.5', 'y1':'1', 'x2':'0.5', 'y2':'0'})
+            offset1 = ET.Element('stop', attrib={'offset':'0%', 'stop-opacity':'1', 'stop-color':'#474747'})
+            offset2 = ET.Element('stop', attrib={'offset':str(step['completion']) + '%', 'stop-opacity':'1', 'stop-color':'#474747'})
+            offset3 = ET.Element('stop', attrib={'offset':str(step['completion']) + '%', 'stop-opacity':'1', 'stop-color':'white'})
+            offset4 = ET.Element('stop', attrib={'offset':'100%', 'stop-opacity':'1', 'stop-color':'white'})
+            grad.append(offset1)
+            grad.append(offset2)
+            grad.append(offset3)
+            grad.append(offset4)
+            svg.append(grad)
 
-ET.ElementTree(svg).write(sys.stdout, encoding='unicode', method='html')
+            #first cirlce is the outline
+            circle = ET.Element('circle', attrib={'cx': str(main_start_x),'cy':str(next_node_y),'r': str(node_r), 'fill':'white','stroke':'#929292'})
+            svg.append(circle)
+
+            #second circle is for the fill
+            circle = ET.Element('circle', attrib={'cx': str(main_start_x),'cy':str(next_node_y),'r': str(node_r - 2), 'fill':'url(#grd' + str(i) + ')'})
+            svg.append(circle)
+
+            text =  ET.Element('text', attrib={'x': str(main_start_x + text_offset_x), 'y':str(next_node_y + text_offset_y)})
+            for line in step['instruction']:
+                tspan =  ET.Element('tspan', attrib={'x':str(main_start_x + text_offset_x),'dy':'19'})
+                tspan.text = line
+                text.append(tspan)
+            svg.append(text)
+            
+            prev_lines = len(step['instruction'])
+        
+    line_end = ' '.join(['M', str(main_start_x), str(main_start_y), 'L', str(main_start_x), str(next_node_y)])
+    line = ET.Element('path', attrib={'d':line_end, 'stroke':'#D7DADA', 'stroke-width':'6'})
+    svg.insert(0,line)
+    svg.attrib['height'] = str(next_node_y + 60)
+    return ET.tostring(svg, encoding='unicode', method='html')
