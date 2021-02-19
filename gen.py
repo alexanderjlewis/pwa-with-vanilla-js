6216d0821cb06057ef229acabd46e2a24cd06994
alexanderjlewis <alex.lewis1991@gmail.com>
2021-02-19 21:58:09 +1100

    commit

--- gen.py
+++ gen.py
@@ -2,15 +2,16 @@ import sys
 from xml.etree import ElementTree as ET
 
 def generate(render_recipe):
-    svg = ET.Element('svg', attrib={'width':'100%', 'height':'100vh'})
+    svg = ET.Element('svg', attrib={'width':'100%'})
 
-    node_spacing_y = 60
-    node_r = 13
+    main_node_spacing_y = 60
+    ingredient_node_spacing_y = 30
+    
+    main_node_r = 13
+    ingredient_square_side = 16
 
-    square_side = 16
-
-    main_path_x = 300
-    ingredient_path_x = 150
+    main_path_x = 240
+    ingredient_path_x = 130
 
     main_start_y = 40
     text_offset_x = 25
@@ -22,12 +23,12 @@ def generate(render_recipe):
         for i, step in enumerate(render_recipe['steps']):
 
             if i > 0:
-                next_node_y += node_spacing_y + ((prev_lines - 1) * 19)
+                next_node_y += main_node_spacing_y + ((prev_lines - 1) * 19)
 
             if step['ingredients']:
                 temp_y = next_node_y - 8
-                for ingredient in step['ingredients']:
-                    square = ET.Element('rect', attrib={'x':str(ingredient_path_x - 8),'y':str(next_node_y - 8),'width':str(square_side),'height':str(square_side),'style':'fill:#3D4242'})
+                for j, ingredient in enumerate(step['ingredients']):
+                    square = ET.Element('rect', attrib={'id':'node_' + str(i) + '_square_' + str(j),'onclick':'changecolor(this)', 'x':str(ingredient_path_x - 8),'y':str(next_node_y - 8),'width':str(ingredient_square_side),'height':str(ingredient_square_side),'fill':'#3D4242','stroke':'#3D4242'})
                     svg.append(square)
                     text = ET.Element('text', attrib={'x': str(ingredient_path_x - 16), 'y':str(next_node_y + 5),'text-anchor':'end','font-size':'smaller'})
                     text.text = ingredient['name']
@@ -36,7 +37,7 @@ def generate(render_recipe):
                     text.text = ingredient['quantity']
                     svg.append(text)
                     
-                    next_node_y += 30
+                    next_node_y += ingredient_node_spacing_y
                 
                 next_node_y += 30
                 s_path = ' '.join(['M', str(main_path_x), str(next_node_y), 'v-10 a20,20 1 0 0 -20,-20 ','H', str(ingredient_path_x + 20) ,' a20,20 0 0 1 -20,-20 V',str(temp_y)])
@@ -56,11 +57,11 @@ def generate(render_recipe):
             svg.append(grad)
 
             #first cirlce is the outline
-            circle = ET.Element('circle', attrib={'cx': str(main_path_x),'cy':str(next_node_y),'r': str(node_r), 'fill':'white','stroke':'#929292'})
+            circle = ET.Element('circle', attrib={'cx': str(main_path_x),'cy':str(next_node_y),'r': str(main_node_r), 'fill':'white','stroke':'#929292'})
             svg.append(circle)
 
             #second circle is for the fill
-            circle = ET.Element('circle', attrib={'cx': str(main_path_x),'cy':str(next_node_y),'r': str(node_r - 2), 'fill':'url(#grd' + str(i) + ')'})
+            circle = ET.Element('circle', attrib={'cx': str(main_path_x),'cy':str(next_node_y),'r': str(main_node_r - 2), 'fill':'url(#grd' + str(i) + ')'})
             svg.append(circle)
 
             text = ET.Element('text', attrib={'x': str(main_path_x + text_offset_x), 'y':str(next_node_y + text_offset_y)})
@@ -69,8 +70,13 @@ def generate(render_recipe):
                 tspan.text = line
                 text.append(tspan)
             svg.append(text)
-            
             prev_lines = len(step['instruction'])
+            
+            #text = ET.Element('text', attrib={'x': str(main_path_x + text_offset_x), 'y':str(next_node_y + text_offset_y)})
+
+            #text = ET.Element('text', attrib={'x': str(main_path_x - text_offset_x), 'y':str(next_node_y + text_offset_y), 'text-anchor':'end'})
+            #text.text = step['time']
+            #svg.append(text)
         
     #oval = ET.Element('path', attrib={'d':'M287,300 v100 a13,13 1 0 0 13,13 a13,13 1 0 0 13,-13 v-100 a-13,13 1 0 0 -13,-13 a13,13 1 0 0 -13,13', 'fill':'white','stroke':'#929292'})
     #svg.append(oval)
@@ -78,5 +84,6 @@ def generate(render_recipe):
     line_end = ' '.join(['M', str(main_path_x), str(main_start_y), 'L', str(main_path_x), str(next_node_y)])
     line = ET.Element('path', attrib={'d':line_end, 'stroke':'#D7DADA', 'stroke-width':'6'})
     svg.insert(0,line)
-    svg.attrib['height'] = str(next_node_y + 60)
+    #svg.attrib['height'] = '1000'
+    svg.attrib['viewBox'] = '0 0 660 ' + str(next_node_y + 60)
     return ET.tostring(svg, encoding='unicode', method='html')
